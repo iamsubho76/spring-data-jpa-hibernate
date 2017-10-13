2e8161a9dfbb562d2359c542fe39bc7a5e404bd7
iamsubho76 <iamsubho76@gmail.com>
2017-10-13 15:36:21 +0530

    updated the project

---
+++ src/main/java/com/org/spring/data/jpa/DatabaseDemoApplication.java
@@ -0,0 +1,12 @@
+package com.org.spring.data.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseDemoApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabaseDemoApplication .class, args);
+	}
+}
