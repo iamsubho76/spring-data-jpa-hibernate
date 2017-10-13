2e0d2d8b371e1b06906942fddd500f5b0442eda3
iamsubho76 <iamsubho76@gmail.com>
2017-10-13 20:16:20 +0530

    Spring JPA code.

--- src/main/java/com/org/spring/data/jpa/entity/Person.java
+++ src/main/java/com/org/spring/data/jpa/entity/Person.java
@@ -2,7 +2,15 @@ package com.org.spring.data.jpa.entity;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Person {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String name;
 	private String location;
---
+++ src/main/java/com/org/spring/data/jpa/jpa/PersonJpaRepository.java
@@ -0,0 +1,21 @@
+package com.org.spring.data.jpa.jpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.org.spring.data.jpa.entity.Person;
+
+@Repository
+@Transactional
+public class PersonJpaRepository {
+
+	@PersistenceContext
+	EntityManager entityManager;
+
+	public Person findById(int id) {
+		return entityManager.find(Person.class, id);
+	}
+}
